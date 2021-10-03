e87d25448a137b0054edae1b2115fed100361bc8
sherylmoser <sherylrmoser@gmail.com>
2021-10-02 18:15:57 -0600

    fixed active list problem
    setActiveList() finished, delete list button functional, render() finished

--- js/app.js
+++ js/app.js
@@ -1,30 +1,66 @@
-
+const lists = []
+let activeListId = 0
 
 function render() {
     //left side HTML
     let listsHtml = '<div class="list-group">';
     // iterate through the lists to get their names
     for (let list of lists) {
-        listsHtml += `<button type="button" onclick="activeList()" class="list-group-item list-group-item-action">${list.name}</button>`;
+        listsHtml += `<button type="button" id="${list.id}" onclick="setActiveList()" class="list-group-item list-group-item-action">${list.name}</button>`;
     };
     listsHtml += '</div>';
     document.getElementById('lists').innerHTML = listsHtml;
-
+    // right side HTML
+    let currentListTitle = `${lists[activeListId].name}`;
+    document.getElementById('currentListTitle').innerText = currentListTitle;
+    // iterate through list to get toDos
+    let toDosHTML = '<ul class="list-group list-group-flush">';
+    let toDos = lists[activeListId].toDos;
+    for (let toDo of toDos) {
+        toDosHTML += `<li class="list-group-item">
+        <input class="form-check-input me-1" type="checkbox">
+        ${toDo.text}
+        </li>`
+    };
+    toDosHTML += '</ul>';
+    document.getElementById('currentToDos').innerHTML = toDosHTML;
+}
+function createListId() {
+    return lists.length;
+}
+// function createToDoId(listIndex) {
+//     return lists[0].toDos.length;
+// }
+function addList() {
+    let newListName = document.getElementById('listName').value;
+    let newList = new ToDoList(createListId(), newListName);
+    lists.push(newList);
+    render();
 }
 
+function addNewToDo() {
+    let newToDoName = document.getElementById('addToDoItem').value;
+    let newToDo = new ToDo(1, newToDoName);
 
-// class for creating app array
-class App {
-    constructor(lists = [], activeList = 0) {
-        this.lists = lists;
-        this.activeList = activeList;
-    }
+    console.log(newToDo);
+    render();
+}
+function setActiveList() {
+    activeListId = event.target.getAttribute('id');
+    render();
+}
+function removeList() {
+    lists.splice(activeListId, 1);
+    activeListId = 0;
+    render();
+
 }
 // class for creating new ToDoList
 class ToDoList {
-    constructor(id, name, toDos = []) {
+    constructor(id, name, activeList = 0, toDos = []) {
         this.id = id;
         this.name = name;
+        this.activeList = activeList;
         this.toDos = toDos;
     }
     addToDo(toDo) {
@@ -45,3 +81,16 @@ class ToDo {
         this.completed = completed;
     }
 }
+// example of how to create lists
+let toDoList = new ToDoList(createListId(), 'Shopping List');
+toDoList.addToDo(new ToDo(1, 'bananas'));
+toDoList.addToDo(new ToDo(2, 'eggs'));
+lists.push(toDoList);
+
+let toDoList2 = new ToDoList(createListId(), 'Cleaning');
+toDoList2.addToDo(new ToDo(1, 'clean craft room'));
+toDoList2.addToDo(new ToDo(2, 'wash windows'));
+lists.push(toDoList2);
+
+
+render()
